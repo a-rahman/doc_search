d6baa994a18278bddd8728ab34f647066dab2629
Abrar Rahman <abrar.a.rahman@gmail.com>
2024-02-04 19:42:37 -0500

    Start on multiple databases

--- document_semantic_search.py
+++ document_semantic_search.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import yaml
 import argparse
 import gradio as gr
@@ -7,19 +9,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", default="configs/semantic_search.yaml")
     args = parser.parse_args()
-    with open(args.config, 'r') as file:
+    with open(args.config, "r") as file:
         config = yaml.safe_load(file)
-    
+
     cm = ContextManager(config)
 
     with gr.Blocks() as demo:
-        file_output = gr.Files()
-        upload_button = gr.UploadButton(
-            "Click to Upload Files",
-            file_count="multiple",
-        )
+        with gr.Row():
+            with gr.Column(scale=1, min_width=600):
+                file_output = gr.Files()
+                upload_button = gr.UploadButton(
+                    "Click to Upload Files",
+                    file_count="multiple",
+                )
+            with gr.Column(scale=1, min_width=600):
+                dirs = glob.glob("doc_index/*")
+                db = gr.Dropdown([os.path.basename(dir) for dir in dirs], label="Databases")
+                new_db = gr.Textbox(label="New Database")
+                add_db = gr.Button("Add Database")
+
         sources_box = gr.TextArea(cm.get_sources, label="Sources")
-        
+
         context_box = gr.TextArea(label="Context")
         msg = gr.Textbox(label="Input")
         clear = gr.Button("Clear")
--- rag.py
+++ rag.py
@@ -5,7 +5,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 context_format = """
-### Context Number: {source_num}
+### Context Rank: {rank_num}
 ### Source: {source} 
 ### Page:{page}
 {content}
@@ -24,7 +24,7 @@ class ContextManager:
         model_kwargs = {"device": config["embedding"]["device"]}
         self.embeddings = HuggingFaceEmbeddings(model_name=config["embedding"]["model"], model_kwargs=model_kwargs)
         self.vectordb = Chroma(
-            persist_directory=config["embedding"]["vectordb"], embedding_function=self.embeddings
+            persist_directory=f"doc_index/{config['embedding']['vectordb']}", embedding_function=self.embeddings
         )
         self.retriever = self.vectordb.as_retriever(search_kwargs={"k": config["embedding"]["max_docs"]})
 
@@ -38,33 +38,38 @@ class ContextManager:
         return "\n".join(sources_list)
 
     def format_context(self, context):
+        matches = []
         output = ""
         seen = set()
-        source_num = 0
+        rank_num = 0
         for doc in context:
             source = os.path.basename(doc.metadata["source"])
             page = doc.metadata["page"]
             content = doc.page_content
             if hash(content) not in seen:
-                source_num+=1
-                output += context_format.format(
-                    source=source, page=page, content=content, source_num=source_num
-                )
+                rank_num+=1
+                matches.append([source, page, content, rank_num])
                 seen.add(hash(content))
+        matches.sort(key=lambda x:x[0])
+        for match in matches:
+            source, page, content, rank_num = match
+            output += context_format.format(
+                source=source, page=page, content=content, rank_num=rank_num
+            )
         return output
 
     def get_context(self, question):
         context = self.retriever.get_relevant_documents(question)
         return self.format_context(context)
 
-    def upload_file(self, files):
+    def upload_file(self, files, db_name="default"):
         file_paths = [file.name for file in files]
         for file_path in file_paths:
             loader = PyPDFLoader(file_path, extract_images=False)
             pages = loader.load_and_split()
             chunks = self.text_splitter.split_documents(pages)
             db = Chroma.from_documents(
-                chunks, embedding=self.embeddings, persist_directory="doc_index"
+                chunks, embedding=self.embeddings, persist_directory=f"doc_index/{db_name}"
             )
             db.persist()
         return file_paths
